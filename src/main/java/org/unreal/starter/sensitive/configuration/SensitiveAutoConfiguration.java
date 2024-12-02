29737b1004f6dfe6328ac9e31ff5910dec9e6175
lincoln <sodlinken@gmail.com>
2024-12-02 10:49:43 +0800

    调整代码中的自动装配

--- src/main/java/org/unreal/starter/sensitive/configuration/SensitiveAutoConfiguration.java
+++ src/main/java/org/unreal/starter/sensitive/configuration/SensitiveAutoConfiguration.java
@@ -2,14 +2,16 @@ package org.unreal.starter.sensitive.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.unreal.starter.sensitive.properties.SensitiveProperties;
 import org.unreal.starter.sensitive.service.SensitiveService;
 
-@Configuration
+@AutoConfiguration
+@ConditionalOnMissingBean(SensitiveService.class)
 @EnableConfigurationProperties({SensitiveProperties.class})
 public class SensitiveAutoConfiguration {
 
@@ -20,13 +22,15 @@ public class SensitiveAutoConfiguration {
     }
 
     @Bean
+    @ConditionalOnMissingBean
     public SensitiveService dataMaskingService(SensitiveProperties sensitiveProperties) {
         return new SensitiveService(sensitiveProperties);
     }
 
     @Bean
     @ConditionalOnBean(ObjectMapper.class)
-    public ObjectMapper dataMaskingObjectMapper(SensitiveService sensitiveService) {
+    @ConditionalOnMissingBean(name = "objectMapper")
+    public ObjectMapper objectMapper(SensitiveService sensitiveService) {
         ObjectMapper objectMapper = new ObjectMapper();
         SimpleModule module = new SimpleModule();
         module.addSerializer(String.class, sensitiveSerializer);
